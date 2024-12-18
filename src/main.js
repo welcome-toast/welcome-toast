70c09393358ddd6af6451652b810769c9cf7da2a
Jintae Song <w.jinttao@gmail.com>
2024-12-18 16:00:07 +0900

    Refactor: update message event origin

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ s.defer = true;
 s.src = "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
 document.head.appendChild(s);
 
-const TARGET_ORIGIN = "http://localhost:5173";
+const TARGET_ORIGIN = "https://welcome-toast.com";
 const SUPABASE_URL = "https://mepmumyanfvgmvjfjpld.supabase.co";
 const SUPABASE_API_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lcG11bXlhbmZ2Z212amZqcGxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1Nzg2MDUsImV4cCI6MjA0OTE1NDYwNX0.HojnVr-YfuBy25jf9qy5DKYkqvdowZ0Pz2FScfIN-04";
