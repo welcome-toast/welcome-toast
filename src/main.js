85d377ba6df7c4ce08d9a0f895a8f0a550bd882e
Jintae Song <w.jinttao@gmail.com>
2024-12-20 16:56:20 +0900

    Feat: set popover image, footer button

--- src/main.js
+++ src/main.js
@@ -226,6 +226,7 @@ function createPopover() {
 
 function setPopover(targetElement, message_title, message_body, image_url) {
   const popover = document.querySelector("#welcomeToastPopover");
+  const popoverImage = document.querySelector("#welcomeToastPopoverImage");
   const popoverHeader = document.querySelector("#welcomeToastPopoverHeader");
   const popoverDescription = document.querySelector("#welcomeToastPopoverDescription");
   const popoverFooter = document.querySelector("#welcomeToastPopoverFooter");
@@ -236,21 +237,24 @@ function setPopover(targetElement, message_title, message_body, image_url) {
 
   popoverHeader.innerHTML = `<span style="font-weight: bold;">${message_title}</span>`;
   popoverDescription.innerHTML = `<span>${message_body}</span>`;
+  popoverFooter.innerHTML = `<div style="width: 60%"></div><button type="button">확인</button>`;
 
   if (image_url) {
-    popoverFooter.innerHTML = `<img src=${image_url} alt="popoverFooter" width="100%" />`;
+    popoverImage.innerHTML = `<img src=${image_url} alt="popoverFooter" width="100%" />`;
   }
 
   if (gapRight < 300) {
     popover.style = `position: absolute; top: ${t.yTarget + t.heightTarget + WHITE_SPACE + window.scrollY}px; right: ${w.widthViewport - t.xTarget - t.widthTarget - WHITE_SPACE}px; flex: auto; flex-direction: column; max-height: 250px; min-width: 200px; max-width: 250px; padding: 15px; border: 1px; margin: 5px; border-radius: 5%; background: #242424; color: white; box-shadow: 0 1px 10px #0006; z-index: 1000000; overflow: clip; overflow-wrap: break-word; word-break: break-all;`;
     popoverHeader.style = "margin-bottom: 5px;";
     popoverDescription.style = "margin-bottom: 5px;";
+    popoverFooter.style = "display: flex; align-items: center; justify-content: space-between;";
     return;
   }
 
   popover.style = `position: absolute; top: ${t.yTarget}px; left: ${xTargetInLayout}px; flex: auto; flex-direction: column; max-height: 250px; min-width: 200px; max-width: 250px; padding: 15px; border: 1px; margin: 5px; border-radius: 5%; background: #242424; color: white; box-shadow: 0 1px 10px #0006; z-index: 1000000; overflow: clip; overflow-wrap: break-word; word-break: break-all;`;
-  popoverHeader.style = "margin-bottom: 5px;";
-  popoverDescription.style = "margin-bottom: 5px;";
+  popoverHeader.style = "margin-bottom: 10px;";
+  popoverDescription.style = "margin-bottom: 10px;";
+  popoverFooter.style = "display: flex; align-items: center; justify-content: space-between;";
   return;
 }
 
